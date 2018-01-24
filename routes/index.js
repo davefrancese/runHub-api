438a23cf7fec707057e2ff13dffd4e920d284cea
dfrancese <davefrancese@gmail.com>
2018-01-24 12:03:49 -0700

    deploy to heroku

--- routes/index.js
+++ routes/index.js
@@ -16,92 +16,45 @@ const url = process.env.PROD_MONGODB || 'mongodb://localhost:27017/'
 
 router.get('/runhub', (req, res, next) => {
   var collection = 'events'
-  // if (process.env.MONGODB_URI ) {
-    // var url = 'mongodb://dfrancese:rundtr@ds113648.mlab.com:13648/runhub'
-    mongo.connect(url, (err, client) => {
-      if(err) {
-        console.log(err);
-      } else {
-        console.log('CONNECTED!!!!');
-        var collection = client.db('runhub').collection('events')
-        collection.find({}).toArray((err, result) => {
-          if(err) {
-            res.send(err)
-          } else if (result.length) {
-            res.send(result)
-          } else {
-            res.send('No Results')
-          }
-        })
-      }
-    })
-
-  // } else {
-  //   var url = 'mongodb://localhost:27017/'
-  //   mongo.connect(url, (err, client) => {
-  //     if(err) {
-  //       console.log(err);
-  //     } else {
-  //       console.log('CONNECTED!!!!');
-  //       var collection = client.db('runhub').collection('events')
-  //       collection.find({}).toArray((err, result) => {
-  //         if(err) {
-  //           res.send(err)
-  //         } else if (result.length) {
-  //           res.send(result)
-  //         } else {
-  //           res.send('No Results')
-  //         }
-  //       })
-  //     }
-  //   })
-  // }
-
+  mongo.connect(url, (err, client) => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log('CONNECTED!!!!');
+      var collection = client.db('runhub').collection('events')
+      collection.find({}).toArray((err, result) => {
+        if(err) {
+          res.send(err)
+        } else if (result.length) {
+          res.send(result)
+        } else {
+          res.send('No Results')
+        }
+      })
+    }
+  })
 })
 
 router.get('/runhub/seed', (req, res, next) => {
   var collection = 'events'
-  if (process.env.MONGODB_URI) {
-    mongo.connect(process.env.MONGODB_URI, (err, client) => {
-      if(err) {
-        console.log(err);
-      } else {
-        console.log('CONNECTED!!!!');
-        var collection = client.db('runhub').collection('events')
-        collection.insertMany(allRaces)
-        .then(result => {
-          if(result.writeError) {
-            res.send(result.writeError)
-          } else if (result) {
-            res.send(result)
-          } else {
-            res.send('No Results')
-          }
-        })
-      }
-    })
-  } else {
-    var url = 'mongodb://localhost:27017/'
-    mongo.connect(url, (err, client) => {
-      if(err) {
-        console.log(err);
-      } else {
-        console.log('CONNECTED!!!!');
-        var collection = client.db('runhub').collection('events')
-        collection.insertMany(allRaces)
-        .then(result => {
-          if(result.writeError) {
-            res.send(result.writeError)
-          } else if (result) {
-            res.send(result)
-          } else {
-            res.send('No Results')
-          }
-        })
-      }
-    })
-  }
-
+  mongo.connect(url, (err, client) => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log('CONNECTED!!!!');
+      var collection = client.db('runhub').collection('events')
+      collection.insertMany(allRaces)
+      .then(result => {
+        if(result.writeError) {
+          res.send(result.writeError)
+        } else if (result) {
+          res.send(result)
+        } else {
+          res.send('No Results')
+        }
+      })
+    }
+  })
 })
 
 
