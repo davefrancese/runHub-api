650dd6c5570aa2d1123cd6a789f6522f56520821
dfrancese <davefrancese@gmail.com>
2018-01-25 16:15:05 -0700

    testing post request req.body.submission

--- routes/index.js
+++ routes/index.js
@@ -86,7 +86,7 @@ router.get('/runhub/seedsub', (req, res, next) => {
     } else {
       console.log('CONNECTED!!!!');
       var collection = client.db('runhub').collection('submits')
-      collection.insert()
+      collection.insert(req.body.submission)
       .then(result => {
         if(result.writeError) {
           res.send(result.writeError)
